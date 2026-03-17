f53a67b2f773039d7a8d09715de01cf0b74370e7
23f3001930 <23f3001930@ds.iitm.study.ac.in>
2026-03-18 00:05:56 +0530

    Initial commit

---
+++ Downloads/mongo-notes-app-pro/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# 🔴 Replace with your MongoDB Atlas connection string
+client = MongoClient("mongodb+srv://admin:admin123@cluster0.owixvlh.mongodb.net/")
+db = client["notes_db"]
+collection = db["notes"]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        note = request.form["note"]
+        if note.strip() != "":
+            collection.insert_one({"text": note})
+        return redirect("/")
+
+    notes = collection.find()
+    return render_template("index.html", notes=notes)
+
+@app.route("/delete/<id>")
+def delete(id):
+    from bson.objectid import ObjectId
+    collection.delete_one({"_id": ObjectId(id)})
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
